48fc7a5b68b6f04931adc75498af1fdd2ff280a1
minemidnight <minemidnightmc@gmail.com>
2017-05-29 18:34:58 -0400

    fix misspell of "function"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = Eris => {
+module.exports = function(Eris) {
 	// Channel additions
 	require("./lib/Channel/awaitMessages.js")(Eris);
 	require("./lib/Channel/createCode.js")(Eris);
