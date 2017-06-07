6f6090a37b6e838f0dd82f70817a556685c36ca8
minemidnight <minemidnight@users.noreply.github.com>
2017-06-06 22:13:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ function isEnabled(mod, options) {
 	else return false;
 }
 
+let loadedAdditions = [];
 function addAddition(addition, Eris, options = {}) {
-	console.log("adding", addition);
 	if(!isEnabled(addition, options)) return;
-	console.log("enabled");
-	console.log(addition.replace(".", "/"));
 
 	let mod = require(`./lib/${addition.replace(".", "/")}`);
-	mod(Eris);
-	mod.deps.forEach(dep => addAddition(dep, Eris));
+	if(!~loadedAdditions.indexOf(addition)) (mod(Eris), loadedAdditions.push(addition));
+	if(mod.deps) mod.deps.forEach(dep => addAddition(dep, Eris));
 }
 
 const fs = require("fs");
-const libdir = require("path").resolve("./lib");
+const libdir = require("path").resolve(__dirname, "lib");
 module.exports = (Eris, options = {}) => {
 	let structs = fs.readdirSync(libdir);
 	structs.forEach(struct => {
